f0c995229081967d0e5e54b2259af9bee040a24a
cmurabito <chandler_murabito@outlook.com>
2026-02-05 12:44:04 -0600

    Implement user account existence check in script
    Added user existence check before creating new accounts.

--- Powershell/Scripts/Bulk_User_Creation.ps1
+++ Powershell/Scripts/Bulk_User_Creation.ps1
@@ -1,7 +1,7 @@
 ## Creates a variable named users from csv file
 $users = Import-Csv -Path "C:\Path\To\YourFile.csv"
 
-## Iterates through csv file and assigns variables/creates users
+## Iterates through csv file and assigns variables
 foreach ($user in  $users) {
     $displayname = $user.FirstName + " " + $user.LastName
     $firstname = $user.FirstName
@@ -10,5 +10,16 @@ foreach ($user in  $users) {
     $upn = $user.Username + "@Lab.local"
     $OU = $user.OU
     $password = "insertpasshere"
-    New-ADUser -Name "$displayname" -DisplayName "$displayname" -SamAccountName $samaccount -UserPrincipalName $upn -GivenName "$firstname" -Surname "$surname" -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $true -server Lab.local
+    
+## Defines username checking va4riable
+    $usercheck = Get-ADUser -Filter "SamAccountName -eq '$samaccount'"
+
+    ## Checks if user account is already in database. If not, creates account.
+    if ($null -ne $usercheck) {
+        New-ADUser -Name "$displayname" -DisplayName "$displayname" -SamAccountName $samaccount -UserPrincipalName $upn -GivenName "$firstname" -Surname "$surname" -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true -Path $OU -ChangePasswordAtLogon $true -server Lab.local
+        Write-Host "$samaccount successfully added to user database."
+    } else {
+        Write-Host "$samaccount already exists in user database."
+    }
+    
 }
