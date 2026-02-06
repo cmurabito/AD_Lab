8ef691a31e2fb94738fe6755de941222a74337c1
Chandler Murabito <chandler_murabito@outlook.com>
2026-02-06 09:21:51 -0600

    Implement check for existing group membership
    Added a check to skip users already in the group before adding them.

--- Powershell/Scripts/Bulk_Group_Assignment.ps1
+++ Powershell/Scripts/Bulk_Group_Assignment.ps1
@@ -5,6 +5,15 @@ foreach ($user in $list) {
     $Username = $user.Username
     $Group = $user.GroupName
     
-    Add-ADGroupMember -Identity $Group -Members $Username
-    Write-Host "$Username has been added to $Group."
+    ## Performs a check to see if user is part of specified group
+    $isUser = Get-ADGroupMember -Identity $Group -Recursive | Where-Object {$_.SamAccountName -eq $userSam}
+
+    ## If the user is already in the group, the user is skipped. Otherwise, the user is added.
+    if ($isUser) {
+        Write-Host "$Username is already a member of $Group. Skipping..."
+    }
+    else {
+        Add-ADGroupMember -Identity $Group -Members $Username
+        Write-Host "$Username has been added to $Group."
+    }
 }
